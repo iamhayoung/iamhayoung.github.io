ac1c4789b1759b87dacd6bf3245e05fa1939d276
Hayoung Gam <hahhyoung@gmail.com>
2021-10-04 21:26:46 +0900

    Apply sass in Gatsby

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,9 +2,9 @@
 import "typeface-montserrat"
 import "typeface-merriweather"
 // normalize CSS across browsers
-import "./src/normalize.css"
+import "./src/styles/_reset.scss"
 // custom CSS styles
-import "./src/style.css"
+import "./src/styles/_global-style.scss"
 
 // Highlighting for code blocks
 import "prismjs/themes/prism.css"
