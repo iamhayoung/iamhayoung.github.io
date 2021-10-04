52594ac40ec5b611f159a0c19950fef08501d47f
Hayoung Gam <hahhyoung@gmail.com>
2021-10-05 01:58:03 +0900

    Create posts page

---
+++ src/pages/posts.js
@@ -0,0 +1,86 @@
+import * as React from "react";
+import { Link, graphql } from "gatsby";
+import Layout from "../components/layout";
+import Seo from "../components/seo";
+
+function Posts({ data, location }) {
+  const siteTitle = data.site.siteMetadata.title;
+  const posts = data.allMarkdownRemark.nodes;
+
+  if (posts.length === 0) {
+    return (
+      <Layout location={location} title={siteTitle}>
+        <Seo title="All posts" />
+        <p>
+          No blog posts found. Add markdown posts to "content/blog" (or the
+          directory you specified for the "gatsby-source-filesystem" plugin in
+          gatsby-config.js).
+        </p>
+      </Layout>
+    );
+  }
+
+  return (
+    <Layout location={location} title={siteTitle}>
+      <Seo title="Posts" />
+      <h1>Posts</h1>
+      <ol style={{ listStyle: `none` }}>
+        {posts.map(post => {
+          const title = post.frontmatter.title || post.fields.slug;
+
+          return (
+            <li key={post.fields.slug}>
+              <article
+                className="post-list-item"
+                itemScope
+                itemType="http://schema.org/Article"
+              >
+                <header>
+                  <h2>
+                    <Link to={post.fields.slug} itemProp="url">
+                      <span itemProp="headline">{title}</span>
+                    </Link>
+                  </h2>
+                  <small>{post.frontmatter.date}</small>
+                </header>
+                <section>
+                  <p
+                    dangerouslySetInnerHTML={{
+                      __html: post.frontmatter.description || post.excerpt,
+                    }}
+                    itemProp="description"
+                  />
+                </section>
+              </article>
+            </li>
+          );
+        })}
+      </ol>
+    </Layout>
+  );
+}
+
+export default Posts;
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      nodes {
+        excerpt
+        fields {
+          slug
+        }
+        frontmatter {
+          date(formatString: "MMMM DD, YYYY")
+          title
+          description
+        }
+      }
+    }
+  }
+`;
