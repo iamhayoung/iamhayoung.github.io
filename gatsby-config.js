dbff15ec95b79375eecf8d5aa16351ad3b2614a3
Hayoung Gam <hahhyoung@gmail.com>
2021-10-06 06:07:22 +0900

    Fix: Google Analytics tracking id 배열 괄호 누락 추가

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,6 +19,7 @@ module.exports = {
           "G-TPDWXRCQEL",
           // "AW-CONVERSION_ID", // Google Ads / Adwords / AW
           // "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)],
+        ],
       },
     },
     `gatsby-plugin-sitemap`,
