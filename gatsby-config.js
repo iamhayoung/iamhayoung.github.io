884ad4a1bd1406087518865a414810a9d8635cd7
Hayoung Gam <hahhyoung@gmail.com>
2021-10-06 06:08:04 +0900

    Add: google-gtag tracking ids comments

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,7 +16,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
         trackingIds: [
-          "G-TPDWXRCQEL",
+          "G-TPDWXRCQEL", // Google Analytics / GA
           // "AW-CONVERSION_ID", // Google Ads / Adwords / AW
           // "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)],
         ],
