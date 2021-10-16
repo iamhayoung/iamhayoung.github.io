6ec87d5a6bfb67fb7e4d525040db738acec7fce6
Hayoung Gam <hahhyoung@gmail.com>
2021-10-16 18:46:11 +0900

    Edit bio font-family -> Montserrat

--- src/components/bio/index.js
+++ src/components/bio/index.js
@@ -8,6 +8,7 @@
 import * as React from "react";
 import { useStaticQuery, graphql } from "gatsby";
 import { StaticImage } from "gatsby-plugin-image";
+import "./style.scss";
 
 const Bio = () => {
   const data = useStaticQuery(graphql`
