01562b7bb65c99cb48cd9b57b0dc56b449f0c9df
Hayoung Gam <hahhyoung@gmail.com>
2021-10-17 22:04:34 +0900

    Create new global header (PC)

--- src/components/global-header/index.js
+++ src/components/global-header/index.js
@@ -1,21 +1,33 @@
 import React from "react";
 import { Link } from "gatsby";
+import { BsGithub } from "react-icons/bs";
+import { HiOutlineMail } from "react-icons/hi";
 import "./style.scss";
 
 function GlobalHeader({ siteTitle }) {
   return (
     <header className="global-header">
-      <h1 className="main-heading">
-        <Link to="/">{siteTitle}</Link>
-      </h1>
-      <nav className="main-header-nav">
-        <Link className="link" to="/about">
-          about
-        </Link>
-        <Link className="link" to="/posts">
-          posts
-        </Link>
-      </nav>
+      <div className="global-header__inner">
+        <nav className="nav">
+          <Link className="nav__link" to="/about">
+            about
+          </Link>
+          <Link className="nav__link" to="/posts">
+            posts
+          </Link>
+        </nav>
+        <h1 className="global-header__site-title">
+          <Link to="/">{siteTitle}</Link>
+        </h1>
+        <div className="actions">
+          <a className="actions__link" href="https://github.com/iamhayoung">
+            <BsGithub />
+          </a>
+          <a className="actions__link" href="mailto:hahhyoung@gmail.com">
+            <HiOutlineMail />
+          </a>
+        </div>
+      </div>
     </header>
   );
 }
--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,23 +1,17 @@
 import * as React from "react";
 import GlobalHeader from "../global-header";
-import PageHeader from "../page-header";
 import GlobalFooter from "../global-footer";
 import "./style.scss";
 
-const Layout = ({ location, title, children }) => {
-  const rootPath = `${__PATH_PREFIX__}/`;
-  const isRootPath = location.pathname === rootPath;
-
+const Layout = ({ title, children }) => {
   return (
-    <div className="global-wrapper">
-      {isRootPath ? (
-        <GlobalHeader siteTitle={title || `Title`} />
-      ) : (
-        <PageHeader siteTitle={title || `Title`} />
-      )}
-      <main>{children}</main>
-      <GlobalFooter />
-    </div>
+    <>
+      <GlobalHeader siteTitle={title || `Title`} />
+      <div className="global-wrapper">
+        <main>{children}</main>
+        <GlobalFooter />
+      </div>
+    </>
   );
 };
 
