ede4cd9dd06188eb692c942294d32f78ebaf3576
Hayoung Gam <hahhyoung@gmail.com>
2021-10-22 03:04:47 +0900

    Add: Mobile 내비게이션 생성

---
+++ src/components/mobileHamburgerMenu/MobileNav.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "gatsby";
+import "./MobileNav.scss";
+
+function MobileNav() {
+  return (
+    <nav className="mobile-nav">
+      <ul className="mobile-nav__list">
+        <li className="mobile-nav__item">
+          <Link className="mobile-nav__link" to="/about">
+            about
+          </Link>
+        </li>
+        <li className="mobile-nav__item">
+          <Link className="mobile-nav__link" to="/posts">
+            posts
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default MobileNav;
