22ece9c9765da04796fa90318fbca15d6ec393b8
Hayoung Gam <hahhyoung@gmail.com>
2021-10-22 04:03:59 +0900

    Add: 모바일 햄버거 메뉴 렌더링 (TODO: 메뉴 오픈 시 트랜지션)

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -3,6 +3,7 @@ import { useMediaQuery } from "react-responsive";
 import PcNav from "./PcNav";
 import Logo from "./Logo";
 import PcActionsMenu from "./PcActionsMenu";
+import MobileHamburgerMenu from "../mobileHamburgerMenu";
 import { BREAKPOINTS } from "../../constants/constants";
 import "./index.scss";
 
@@ -26,7 +27,10 @@ function Header({ siteTitle }) {
           {isMobile && <Logo siteTitle={siteTitle} />}
         </div>
         {!isMobile && <Logo siteTitle={siteTitle} />}
-        <div className="right-section">{!isMobile && <PcActionsMenu />}</div>
+        <div className="right-section">
+          {!isMobile && <PcActionsMenu />}
+          {isMobile && <MobileHamburgerMenu />}
+        </div>
       </div>
     </header>
   );
